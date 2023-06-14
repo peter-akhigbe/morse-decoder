5fa5a4eaf13167ca78842452b83b317c54d9c3b0
Peter Akhigbe <akhigbe37@gmail.com>
2023-06-14 16:46:22 +0100

    Write script for morse message decoder

---
+++ decoder-morse-message.rb
@@ -0,0 +1,60 @@
+class MorseCodeDecoder
+  MORSE_CODE = {
+    '.-' => 'A',
+    '-...' => 'B',
+    '-.-.' => 'C',
+    '-..' => 'D',
+    '.' => 'E',
+    '..-.' => 'F',
+    '--.' => 'G',
+    '....' => 'H',
+    '..' => 'I',
+    '.---' => 'J',
+    '-.-' => 'K',
+    '.-..' => 'L',
+    '--' => 'M',
+    '-.' => 'N',
+    '---' => 'O',
+    '.--.' => 'P',
+    '--.-' => 'Q',
+    '.-.' => 'R',
+    '...' => 'S',
+    '-' => 'T',
+    '..-' => 'U',
+    '...-' => 'V',
+    '.--' => 'W',
+    '-..-' => 'X',
+    '-.--' => 'Y',
+    '--..' => 'Z',
+    '.----' => '1',
+    '..---' => '2',
+    '...--' => '3',
+    '....-' => '4',
+    '.....' => '5',
+    '-....' => '6',
+    '--...' => '7',
+    '---..' => '8',
+    '----.' => '9',
+    '-----' => '0'
+  }.freeze
+
+  def self.decode_char(morse_char)
+    MORSE_CODE[morse_char]
+  end
+
+  def self.decode_word(morse_word)
+    decoded_word = ''
+    morse_word.split.each do |morse_char|
+      decoded_word += decode_char(morse_char)
+    end
+    decoded_word
+  end
+
+  def self.decode_message(morse_message)
+    decoded_message = ''
+    morse_message.split('   ').each do |morse_word|
+      decoded_message += "#{decode_word(morse_word)} "
+    end
+    decoded_message.strip
+  end
+end
